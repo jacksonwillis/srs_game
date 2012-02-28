9f0ecff9c3f5913ec7e4700596a57c61578c23f7
Jackson <railsmail42@gmail.com>
2012-02-28 16:46:23 -0600

    Remove SRSGame::Tia

--- lib/srs_game/tia.rb
+++ lib/srs_game/tia.rb
@@ -4,16 +4,9 @@
 
 module SRSGame::Tia
   def main_room
-    main = L.new(:name => "Main Room")
-    main.east = L.new(:name => "East Room")
-    main.east.south = L.new(:name => "South East Room")
-    main.east.south.south = L.new(:name => "South South East Room")
-    main.west = L.new(:name => "West Room")
-    main.west.west = L.new(:name => "Far West Room")
-    main.west.west.north = L.new(:name => "North West Room")
-    main
+    exit
   end
   def greeting
-    base64_zlib_inflate "eJydUVEKQDEI+vcUgve/42MbWy2DweunMjEjUCPIk0fFFbuNubBA8eRTJ2hmp6a5S8OcKFniDoVy7Ddl9dSnCQHFRz6jjGBrGtJUvCAUl/97mn68ItPSvQ1sqhvS7fbJXzg+1sVJ3g=="
+    "SRSGame::Tia is not yet implemented."
   end
 end
