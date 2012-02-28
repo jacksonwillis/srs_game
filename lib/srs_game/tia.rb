ab08577ad10dab6bb3a67641507ef3b3d967a081
Jackson <railsmail42@gmail.com>
2012-02-28 16:50:18 -0600

    Make SRSGame::Tia exit

--- lib/srs_game/tia.rb
+++ lib/srs_game/tia.rb
@@ -3,10 +3,7 @@
 # This file is part of SRS GAME <http://github.com/jacksonwillis/srs_game/>.
 
 module SRSGame::Tia
-  def main_room
-    exit
-  end
-  def greeting
-    "SRSGame::Tia is not yet implemented."
-  end
 end
+
+puts "SRSGame::Tia is not yet implemented."
+exit
