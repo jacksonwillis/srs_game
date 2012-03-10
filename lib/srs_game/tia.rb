dd47709bd59e2560e4e4aa861043a3059d515c3e
Jackson Willis <railsmail42@gmail.com>
2012-03-10 10:29:56 -0600

    Make tia.rb work when called directly

--- lib/srs_game/tia.rb
+++ lib/srs_game/tia.rb
@@ -3,5 +3,5 @@
 # This file is part of SRS GAME <http://github.com/jacksonwillis/srs_game/>.
 
 # Not yet implemented
-module SRSGame::Tia end
+module SRSGame module Tia end end
 raise "SRSGame::Tia is not yet implemented."
