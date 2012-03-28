a1606eb507264eaf0eee0c8867ff31f4acf66ccf
Jackson <railsmail42@gmail.com>
2012-03-28 16:25:15 -0500

    Add simplecov to cucumber env

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,9 @@
 # -*- coding: UTF-8 -*-
 # This file is part of SRS GAME <http://github.com/jacksonwillis/srs_game/>.
 
+require "simplecov"
+SimpleCov.start
+
 require "cucumber/formatter/unicode"
 require "rspec/expectations"
 
