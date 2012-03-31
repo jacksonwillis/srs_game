c3d59868f82f29e6d6e0b9bdcad31648490f89ae
Jackson <railsmail42@gmail.com>
2012-03-31 13:51:55 -0500

    Standardize version [ci skip]

--- lib/srs_game/version.rb
+++ lib/srs_game/version.rb
@@ -1,3 +1,7 @@
+#!/usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+# This file is part of SRS GAME <http://github.com/jacksonwillis/srs_game/>.
+
 module SRSGame
-  VERSION = "0.Alexis"
+  VERSION = "0.1.Alexis"
 end
